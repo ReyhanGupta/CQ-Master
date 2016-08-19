1d10e2403031166bea5214254a76db23bc352a2f
Devesh Saini <deveshsaini96@gmail.com>
2016-08-20 00:35:24 +0530

    Write Mode List View

--- app/src/main/java/com/devesh/intern/WriteMode.java
+++ app/src/main/java/com/devesh/intern/WriteMode.java
@@ -1,6 +1,7 @@
 package com.devesh.intern;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
@@ -8,6 +9,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
 /**
@@ -15,6 +23,9 @@ import android.view.ViewGroup;
  */
 public class WriteMode extends Fragment {
 
+    ListView listView;
+    ListAdapter adapter;
+    ArrayList<Info> databaseList;
 
     public WriteMode() {
         // Required empty public constructor
@@ -26,19 +37,124 @@ public class WriteMode extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_write_mode, container, false);
-        Log.d("Write","View Reached");
+
+        
+
+
+
         return view;
     }
 
-    public class RecyclerViewHolder extends RecyclerView.ViewHolder{
+    public class ListAdapter extends BaseAdapter{
+
+        private ArrayList<Info> mList;
+
+        public ListAdapter(ArrayList<Info> mList) {
+            this.mList = mList;
+        }
+
+        class Holder{
+
+
+            TextView title,genre,synopsis,date,time;
+            ProgressBar progressBar;
+            ImageView imageView;
 
 
 
-        public RecyclerViewHolder(View itemView) {
-            super(itemView);
+        }
+
+
+
+        @Override
+        public int getCount() {
+            return mList.size();
+        }
+
+        @Override
+        public Info getItem(int i) {
+            return mList.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            return position==0?0:1;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            Holder holder=new Holder();
+            LayoutInflater inflater = getActivity().getLayoutInflater();
+            if(view==null) {
+                if (i == 0)
+                    view = inflater.inflate(R.layout.create_new, null);
+                else {
+                    view = inflater.inflate(R.layout.writemode_items, null);
+                    
+
+
+                    holder.title = (TextView) view.findViewById(R.id.tv1);
+                    holder.genre = (TextView) view.findViewById(R.id.tv2);
+                    holder.synopsis = (TextView) view.findViewById(R.id.tv5);
+                    holder.date = (TextView) view.findViewById(R.id.tv3);
+                    holder.time = (TextView) view.findViewById(R.id.tv4);
+
+                    holder.progressBar = (ProgressBar) view.findViewById(R.id.bar1);
+
+                    holder.imageView = (ImageView) view.findViewById(R.id.profile_image);
+
+                    view.setTag(holder);
+
+                }
+            }else{
+                if(i==0)
+                    view = inflater.inflate(R.layout.create_new,null);
+                else{
+                    holder = (Holder) view.getTag();
+                }
+            }
+
+
+            if(i!=0){
+
+                final Info item =  getItem(i-1);
+
+                holder.title.setText(item.name);
+                holder.genre.setText(item.genre);
+                
+                holder.date.setText(item.date);
+                holder.time.setText(String.valueOf(item.time));
+
+                view.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(getContext(),ViewStory.class);
+                        intent.putExtra("Name",item.name);
+                        intent.putExtra("Genre",item.genre);
+                        intent.putExtra("Story",item.story);
+                        startActivityForResult(intent,1234);
+                    }
+                });
+
+
+
+            }
+
+            return view;
         }
     }
 
-    public class RecyclerAdapter extends Recy
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        if((requestCode==1234)&&(resultCode==-1)){
+           // bar.setProgress(0);
+        }
+    }
 }
